f0e650683fe884926098229f27df30ac7bb1d9d7
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-04 23:46:53 +0500

    Initial Commit

---
+++ Combine&SwiftUI_1/Combine_SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_SwiftUI_1App.swift
+//  Combine&SwiftUI_1
+//
+//  Created by Вячеслав Квашнин on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Combine_SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
